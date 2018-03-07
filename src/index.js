75f7256884b6b0e4975b1609963d3831e2a18a04
Joao <joao.antunes@present-technologies.com>
2018-03-07 14:04:53 +0000

    Base strucutre

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 // React
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, withRouter } from 'react-router-dom';
 
 // D2
-import { getManifest } from 'd2/lib/d2';
+import { getManifest, getUserSettings } from 'd2/lib/d2';
 import D2UIApp from 'd2-ui/lib/app/D2UIApp';
 
 // logging
 import log from 'loglevel';
+
+// Internatinalization: i18next
+import { I18nextProvider, translate } from 'react-i18next';
+import i18n from './i18n';
 
 import './index.css';
 import App from './App';
@@ -15,26 +20,45 @@ import appTheme from './theme';
 
 import registerServiceWorker from './registerServiceWorker';
 
+const AppComponent = withRouter(translate()(App));
+
 log.setLevel(process.env.NODE_ENV === 'production' ? log.levels.INFO : log.levels.DEBUG);
+
+const configLocale = (userSettings) => {
+    const uiLocale = userSettings.keyUiLocale;
+    if (uiLocale && uiLocale !== 'en') {
+        i18n.changeLanguage(uiLocale);
+    }
+    sessionStorage.setItem('uiLocale', uiLocale || 'en');
+};
 
 // init d2
 getManifest('manifest.webapp').then((manifest) => {
+    const api = process.env.REACT_APP_DHIS2_API_VERSION ? `/${process.env.REACT_APP_DHIS2_API_VERSION}` : '/';
     const baseUrl =
-        process.env.NODE_ENV === 'production'
-            ? manifest.getBaseUrl()
-            : process.env.REACT_APP_DHIS2_BASE_URL;
+      process.env.NODE_ENV === 'production'
+          ? `${manifest.getBaseUrl()}/api/${manifest.dhis2.apiVersion}`
+          : `${process.env.REACT_APP_DHIS2_BASE_URL}/api${api}`;
 
     ReactDOM.render(
         <D2UIApp
             muiTheme={appTheme}
             initConfig={{
-                baseUrl: `${baseUrl}/api/${process.env.REACT_APP_DHIS2_API_VERSION}`,
+                baseUrl,
+                schemas: [
+                    'organisationUnit',
+                    'dataSet',
+                ],
             }}
         >
-            <App />
+            <I18nextProvider i18n={i18n}>
+                <HashRouter>
+                    <AppComponent />
+                </HashRouter>
+            </I18nextProvider>
         </D2UIApp>,
-        document.getElementById('root'),
+        document.getElementById('app'),
     );
-});
+}).then(getUserSettings).then(configLocale);
 
 registerServiceWorker();
