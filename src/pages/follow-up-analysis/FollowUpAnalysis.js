bb2bfc59b5e2f9fd2b3b3d6c8f4c2d93368ad5e9
MDHomem <mdhomem@gmail.com>
2018-03-20 13:52:02 +0000

    Toggle between home and list screen

--- src/pages/follow-up-analysis/FollowUpAnalysis.js
+++ src/pages/follow-up-analysis/FollowUpAnalysis.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 // Material UI
+import { FontIcon, IconButton } from 'material-ui';
 import { Card, CardText } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 import DatePicker from 'material-ui/DatePicker';
@@ -21,6 +22,7 @@ import { i18nKeys } from '../../i18n';
 // styles
 import cssClasses from '../Page.css';
 import pageStyles from '../PageStyles';
+import FollowUpAnalysisTable from './FollowUpAnalysisTable';
 
 class FollowUpAnalysis extends Page {
     constructor() {
@@ -28,9 +30,12 @@ class FollowUpAnalysis extends Page {
 
         this.state = {
             organisationUnitId: null,
+            showTable: false,
         };
 
         this.organisationUnitChanged = this.organisationUnitChanged.bind(this);
+        this.getFollowUpList = this.getFollowUpList.bind(this);
+        this.backToFollowHome = this.backToFollowHome.bind(this);
     }
 
     organisationUnitChanged(organisationUnitId) {
@@ -39,48 +44,70 @@ class FollowUpAnalysis extends Page {
         });
     }
 
+    getFollowUpList() {
+        this.setState({ ...this.state, showTable: true });
+    }
+
+    backToFollowHome() {
+        this.setState({ ...this.state, showTable: false });
+    }
+
     render() {
         const translator = this.context.translator;
         return (
             <div className="page-wrapper">
                 <h1>
-                    {translator(i18nKeys.followUpAnalysis.header)}
+                    <IconButton onClick={this.backToFollowHome}>
+                        <FontIcon
+                            className={'material-icons'}
+                        >
+                            arrow_back
+                        </FontIcon>
+                    </IconButton>
+                    <span>{translator(i18nKeys.followUpAnalysis.header)}</span>
                     <PageHelper
                         sectionDocsKey={getDocsKeyForSection(this.props.sectionKey)}
                     />
                 </h1>
-                <Card>
-                    <CardText>
-                        <div className="row">
-                            <div className={classNames('col-md-6', cssClasses.section)}>
-                                <span>{translator(i18nKeys.followUpAnalysis.form.organisationUnit)}</span>
-                                <AvailableOrganisationUnitsTree
-                                    onChange={this.organisationUnitChanged}
+                {
+                    !this.state.showTable ? (
+                        <Card>
+                            <CardText>
+                                <div className="row">
+                                    <div className={classNames('col-md-6', cssClasses.section)}>
+                                        <span>{translator(i18nKeys.followUpAnalysis.form.organisationUnit)}</span>
+                                        <AvailableOrganisationUnitsTree
+                                            onChange={this.organisationUnitChanged}
+                                        />
+                                    </div>
+                                    <div className={classNames('col-md-6', cssClasses.section)}>
+                                        <DatasetsForOrganisationUnitSelect
+                                            organisationUnitId={this.state.organisationUnitId}
+                                        />
+                                        <DatePicker
+                                            textFieldStyle={pageStyles.inputForm}
+                                            floatingLabelText={translator(i18nKeys.followUpAnalysis.form.startDate)}
+                                            defaultDate={new Date()}
+                                        />
+                                        <DatePicker
+                                            textFieldStyle={pageStyles.inputForm}
+                                            floatingLabelText={translator(i18nKeys.followUpAnalysis.form.endDate)}
+                                            defaultDate={new Date()}
+                                        />
+                                    </div>
+                                </div>
+                                <RaisedButton
+                                    className={cssClasses.mainButton}
+                                    primary={Boolean(true)}
+                                    label={translator(i18nKeys.followUpAnalysis.actionButton)}
+                                    onClick={this.getFollowUpList}
                                 />
-                            </div>
-                            <div className={classNames('col-md-6', cssClasses.section)}>
-                                <DatasetsForOrganisationUnitSelect
-                                    organisationUnitId={this.state.organisationUnitId}
-                                />
-                                <DatePicker
-                                    textFieldStyle={pageStyles.inputForm}
-                                    floatingLabelText={translator(i18nKeys.followUpAnalysis.form.startDate)}
-                                    defaultDate={new Date()}
-                                />
-                                <DatePicker
-                                    textFieldStyle={pageStyles.inputForm}
-                                    floatingLabelText={translator(i18nKeys.followUpAnalysis.form.endDate)}
-                                    defaultDate={new Date()}
-                                />
-                            </div>
-                        </div>
-                        <RaisedButton
-                            className={cssClasses.mainButton}
-                            primary={Boolean(true)}
-                            label={translator(i18nKeys.followUpAnalysis.actionButton)}
-                        />
-                    </CardText>
-                </Card>
+                            </CardText>
+                        </Card>
+                    ) : (
+                        <FollowUpAnalysisTable elements={[]} />
+                    )
+                }
             </div>
         );
     }
