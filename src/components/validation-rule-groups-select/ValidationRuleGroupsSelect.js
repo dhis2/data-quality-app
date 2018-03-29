6a8cf31fd9d1db45a4812246e12e84a40df0aa7e
MDHomem <mdhomem@gmail.com>
2018-03-29 10:56:34 +0100

    Fix selected group information

--- src/components/validation-rule-groups-select/ValidationRuleGroupsSelect.js
+++ src/components/validation-rule-groups-select/ValidationRuleGroupsSelect.js
@@ -7,71 +7,90 @@ import MenuItem from 'material-ui/MenuItem';
 
 import { i18nKeys } from '../../i18n';
 
+export const ALL_VALIDATION_RULE_GROUPS_ID = -1;
+
 class ValidationRuleGroupsSelect extends PureComponent {
-  static propTypes = {
-      style: PropTypes.object,
-  }
+    static propTypes = {
+        style: PropTypes.object,
+        onChange: PropTypes.func.isRequired,
+    }
 
-  static defaultProps = {
-      style: {},
-  }
+    static defaultProps = {
+        style: {},
+    }
 
-  static contextTypes = {
-      d2: PropTypes.object,
-      translator: PropTypes.func,
-  }
+    static contextTypes = {
+        d2: PropTypes.object,
+        translator: PropTypes.func,
+    }
 
-  constructor() {
-      super();
+    constructor() {
+        super();
 
-      this.state = {
-          validationRuleGroups: null,
-      };
-  }
+        this.state = {
+            validationRuleGroups: null,
+            selected: -1,
+        };
 
-  componentDidMount() {
-      const d2 = this.context.d2;
-      if (this.state.validationRuleGroups == null) {
-          d2.models.validationRuleGroup.list({
-              paging: false,
-              fields: 'id,displayName',
-          }).then((validationRuleGroupsResponse) => {
-              this.setState({
-                  validationRuleGroups: validationRuleGroupsResponse.toArray(),
-              });
-          }).catch(() => {
-              // TODO
-          });
-      }
-  }
+        this.onChange = this.onChange.bind(this);
+    }
 
-  render() {
-      const translator = this.context.translator;
-      return (
-          <SelectField
-              style={this.props.style}
-              floatingLabelText={
-                  translator(i18nKeys.validationRuleGroupsSelect.validationRuleGroupLabel)
-              }
-              value={-1}
-          >
-              <MenuItem
-                  key={'-1'}
-                  value={-1}
-                  primaryText={translator(i18nKeys.validationRuleGroupsSelect.allValidationRulesOption)}
-              />
-              {this.state.validationRuleGroups &&
-                this.state.validationRuleGroups.map(item => (
-                    <MenuItem
-                        key={item.id}
-                        value={item.id}
-                        primaryText={item.displayName}
-                    />
-                ))
-              }
-          </SelectField>
-      );
-  }
+    componentDidMount() {
+        const d2 = this.context.d2;
+        if (this.state.validationRuleGroups == null) {
+            d2.models.validationRuleGroup.list({
+                paging: false,
+                fields: 'id,displayName',
+            }).then((validationRuleGroupsResponse) => {
+                this.setState({
+                    validationRuleGroups: validationRuleGroupsResponse.toArray(),
+                });
+            }).catch(() => {
+                // TODO
+            });
+        }
+    }
+
+    onChange(event, index, value) {
+        this.setState({
+            selected: value,
+            selectedName: value === ALL_VALIDATION_RULE_GROUPS_ID ?
+                i18nKeys.validationRuleGroupsSelect.allValidationRulesOption :
+                this.state.validationRuleGroups[index].displayName,
+        });
+
+        this.props.onChange(event, index, value);
+    }
+
+    render() {
+        const translator = this.context.translator;
+        return (
+            <SelectField
+                style={this.props.style}
+                floatingLabelText={
+                    translator(i18nKeys.validationRuleGroupsSelect.validationRuleGroupLabel)
+                }
+                onChange={this.onChange}
+                value={this.state.selected}
+            >
+                <MenuItem
+                    key={'-1'}
+                    value={-1}
+                    primaryText={translator(i18nKeys.validationRuleGroupsSelect.allValidationRulesOption)}
+                />
+                {
+                    this.state.validationRuleGroups &&
+                    this.state.validationRuleGroups.map(item => (
+                        <MenuItem
+                            key={item.id}
+                            value={item.id}
+                            primaryText={item.displayName}
+                        />
+                    ))
+                }
+            </SelectField>
+        );
+    }
 }
 
 export default ValidationRuleGroupsSelect;
