fed45ad6f3357118da021798b7312539551b2761
Jen Jones Arnesen <jennifer@dhis2.org>
2019-03-18 11:46:19 +0300

    bundle material-icons/css instead of getting from the internet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'typeface-roboto'; // eslint-disable-line
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
