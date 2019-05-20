327fecaab2419f5b1a9b896ce32041d41cba6983
ismay <ismay@ismaywolff.nl>
2019-05-20 12:21:59 +0200

    Stop using manifest for api version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, withRouter } from 'react-router-dom';
-import { getManifest, getUserSettings } from 'd2/lib/d2';
+import { getUserSettings } from 'd2/lib/d2';
 import D2UIApp from 'd2-ui/lib/app/D2UIApp';
 import log from 'loglevel';
 import { configI18n } from './configI18n';
@@ -10,42 +10,35 @@ import App from './App';
 import appTheme from './theme';
 import * as serviceWorker from './serviceWorker';
 
-const AppComponent = withRouter(App);
-
 log.setLevel(process.env.NODE_ENV === 'production' ? log.levels.INFO : log.levels.DEBUG);
 
-const configurations = (userSettings) => {
+const AppComponent = withRouter(App);
+const baseUrl = `${process.env.REACT_APP_DHIS2_BASE_URL}/api/${process.env.REACT_APP_DHIS2_API_VERSION}`;
+
+ReactDOM.render(
+    <D2UIApp
+        muiTheme={appTheme}
+        initConfig={{
+            baseUrl,
+            schemas: [
+                'organisationUnit',
+                'dataSet',
+                'validationRuleGroup',
+            ],
+        }}
+    >
+        <HashRouter>
+            <AppComponent />
+        </HashRouter>
+    </D2UIApp>,
+    document.getElementById('root'),
+    );
+
+getUserSettings().then((userSettings) => {
     const uiLocale = userSettings.keyUiLocale;
     sessionStorage.setItem('uiLocale', uiLocale || 'en');
 
     configI18n(userSettings);
-};
-
-getManifest('manifest.webapp').then((manifest) => {
-    const api = process.env.REACT_APP_DHIS2_API_VERSION ? `/${process.env.REACT_APP_DHIS2_API_VERSION}` : '/';
-    const baseUrl =
-      process.env.NODE_ENV === 'production'
-          ? `${manifest.getBaseUrl()}/api/${manifest.dhis2.apiVersion}`
-          : `${process.env.REACT_APP_DHIS2_BASE_URL}/api${api}`;
-
-    ReactDOM.render(
-        <D2UIApp
-            muiTheme={appTheme}
-            initConfig={{
-                baseUrl,
-                schemas: [
-                    'organisationUnit',
-                    'dataSet',
-                    'validationRuleGroup',
-                ],
-            }}
-        >
-            <HashRouter>
-                <AppComponent />
-            </HashRouter>
-        </D2UIApp>,
-        document.getElementById('root'),
-    );
-}).then(getUserSettings).then(configurations);
+})
 
 serviceWorker.register();
