19ee4b1bb273c6b664a2d66438e53b6dbf2c1c3f
Hendrik de Graaf <hendrik@dhis2.org>
2019-10-10 14:52:10 +0200

    fix(deps): import Roboto and material-design-icons in App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import 'typeface-roboto'
+import 'material-design-icons-iconfont'
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
