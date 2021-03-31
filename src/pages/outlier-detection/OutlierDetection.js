63b2f67d9cae5557a04280fc6dfaf88a8d2405e5
Médi-Rémi Hashim <mediremi@gmail.com>
2021-03-31 15:41:41 +0100

    fix: add buttons to clear optional data start/end dates

--- src/pages/outlier-detection/OutlierDetection.js
+++ src/pages/outlier-detection/OutlierDetection.js
@@ -154,12 +154,12 @@ class OutlierDetection extends Page {
         this.setState({ endDate: new Date(date) })
     }
 
-    handleDataStartDateChange = (event, date) => {
-        this.setState({ dataStartDate: new Date(date) })
+    handleDataStartDateChange = date => {
+        this.setState({ dataStartDate: date && new Date(date) })
     }
 
-    handleDataEndDateChange = (event, date) => {
-        this.setState({ dataEndDate: new Date(date) })
+    handleDataEndDateChange = date => {
+        this.setState({ dataEndDate: date && new Date(date) })
     }
 
     handleMaxResultsChange = (event, index, value) => {
