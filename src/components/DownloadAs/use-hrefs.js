b4a2fb2a1226dba384d6acf426ad6e3188942ba5
Médi-Rémi Hashim <mediremi@gmail.com>
2021-07-02 09:10:34 +0100

    refactor: replace d2 with useConfig in useHrefs hook

--- src/components/DownloadAs/use-hrefs.js
+++ src/components/DownloadAs/use-hrefs.js
@@ -1,15 +1,15 @@
-import { useD2 } from '@dhis2/app-runtime-adapter-d2'
+import { useConfig } from '@dhis2/app-runtime'
 
 const useHrefs = ({ endpoint, fileTypes, queryStr }) => {
-    const { d2 } = useD2()
-    const { baseUrl } = d2.Api.getApi()
+    const { baseUrl, apiVersion } = useConfig()
     const timestamp = Date.now()
     // The new outlierDetection endpoint produces the correct report
     // based on query params, but older endpoints only take a timestamp
     queryStr = queryStr || `t=${timestamp}`
 
     return fileTypes.reduce((acc, type) => {
-        acc[type] = `${baseUrl}${endpoint}.${type}?${queryStr}`
+        const downloadUrl = `${baseUrl}/api/${apiVersion}/${endpoint}.${type}?${queryStr}`
+        acc[type] = downloadUrl
         return acc
     }, {})
 }
