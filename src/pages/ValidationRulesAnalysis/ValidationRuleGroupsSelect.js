b3866ab9f1729f3472b485cdc04e2236a66a1e7a
Médi-Rémi Hashim <mediremi@gmail.com>
2021-07-15 11:07:06 +0100

    refactor: simplify validation rule groups select query

--- src/pages/ValidationRulesAnalysis/ValidationRuleGroupsSelect.js
+++ src/pages/ValidationRulesAnalysis/ValidationRuleGroupsSelect.js
@@ -21,18 +21,14 @@ const query = {
 }
 
 const ValidationRuleGroupsSelect = ({ onChange }) => {
-    const [validationRuleGroups, setValidationRuleGroups] = useState([
-        ALL_VALIDATION_RULE_GROUPS_OPTION,
-    ])
     const [selected, setSelected] = useState(ALL_VALIDATION_RULE_GROUPS_ID)
-    const { loading, error } = useDataQuery(query, {
-        onComplete: data => {
-            setValidationRuleGroups([
-                ALL_VALIDATION_RULE_GROUPS_OPTION,
-                ...data.validationRuleGroups.validationRuleGroups,
-            ])
-        },
-    })
+    const { loading, error, data } = useDataQuery(query)
+    const validationRuleGroups = data
+        ? [
+              ALL_VALIDATION_RULE_GROUPS_OPTION,
+              ...data.validationRuleGroups.validationRuleGroups,
+          ]
+        : [ALL_VALIDATION_RULE_GROUPS_OPTION]
 
     const handleChange = ({ selected }) => {
         setSelected(selected)
