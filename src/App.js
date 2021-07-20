707c4c83749e86332243e2c981f685d3b9680f3d
Médi-Rémi Hashim <mediremi@gmail.com>
2021-07-20 16:39:54 +0100

    fix: make src/locales/index.js first import in src/App.js
    This ensures that translations are loaded before src/pages/sections.conf.js
    is imported, which is required due to the static nature of the way translations
    are handled in that file.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+// Due to the way translations are handled in src/pages/sections.conf.js,
+// src/locales/index.js must be the first import in this file
+import './locales'
+import './grid.css'
+
 import { CssVariables } from '@dhis2/ui'
 import { MuiThemeProvider } from 'material-ui/styles'
 import React from 'react'
@@ -5,8 +10,6 @@ import { HashRouter } from 'react-router-dom'
 import 'material-design-icons-iconfont'
 import App from './components/App/App'
 import appTheme from './theme'
-import './grid.css'
-import './locales'
 
 const AppWrapper = () => (
     <>
